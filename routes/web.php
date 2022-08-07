5e50e24c952351846f7179c3d650880f1c43c833
AptsiauriV <vazha.aptsiauri@lavapi.com>
2022-08-07 15:40:39 +0400

    Apply Auth Middleware

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('outfit', OutfitController::class);
+Route::resource('outfit', OutfitController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
