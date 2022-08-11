220a50404dbf37978816edf62be8df1e121fce90
AptsiauriV <vazha.aptsiauri@lavapi.com>
2022-08-11 15:03:22 +0400

    Added outfits relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable
 
     public function outfits(): HasMany
     {
-        return $this->HasMany(Clothing::class);
+        return $this->HasMany(Outfit::class);
     }
 }
