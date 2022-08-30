afabfe27c5f0626d3db25ab49e0adf989b58dcbc
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2022-08-30 21:45:06 +0400

    Make Tags Optional

--- app/Http/Requests/StoreClothingRequest.php
+++ app/Http/Requests/StoreClothingRequest.php
@@ -29,7 +29,7 @@ class StoreClothingRequest extends FormRequest
         return [
             'category' => 'required|exists:categories,id',
             'season' => 'required|exists:seasons,id',
-            'tags' => 'nullable|string',
+            'tags' => 'required|string',
             'image' => 'required_without:image_url|mimes:jpg,jpeg,png',
             'image_url' => 'nullable|url'
         ];
